b8bde9ee94d9844dc1d2f18ac654840c486031fb
Jocsan Rodriguez <jasub270@gmail.com>
2025-06-26 19:14:05 -0400

    saved

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
 
   useEffect(() => {
     fetchAllTasks();
-  }, []);
+  }, []); 
 
   return (
     <div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production", // change to production for deploy build
   entry: "./src/App.jsx",
   output: {
     path: path.resolve(__dirname, "docs"),
     filename: "main.js",
+    publicPath: "./", // important for GitHub Pages relative paths
   },
   module: {
     rules: [
@@ -28,6 +30,11 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // your source HTML
+    }),
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, "docs"),
