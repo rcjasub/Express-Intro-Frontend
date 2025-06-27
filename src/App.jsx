49a27672060953de89d22115e3220dbb888f39a3
Jocsan Rodriguez <jasub270@gmail.com>
2025-06-26 21:44:48 -0400

    saving

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
 
   async function fetchAllTasks() {
     try {
-      const response = await axios.get("https://express-intro-starting-point-fvimqno6j.vercel.app/api/tasks");
+      const response = await axios.get("https://express-intro-starting-point-fvimqno6j.vercel.app/");
       setTasks(response.data);
     } catch (error) {
       console.error("Error fetching tasks:", error);
